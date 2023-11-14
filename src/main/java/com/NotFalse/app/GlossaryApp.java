5ecf62a20c765e4f5bbecf8f62ebd81598ab8f4b
Michal Johnson <johnsmic@students.zhaw.ch>
2023-11-14 21:31:59 +0100

    implmented glossary junit

--- src/main/java/com/NotFalse/app/GlossaryApp.java
+++ src/main/java/com/NotFalse/app/GlossaryApp.java
@@ -1,47 +1,82 @@
 package com.NotFalse.app;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 import java.util.TreeMap;
 
-public class Glossary {
+
+public class GlossaryApp {
 
     private TreeMap<String, ArrayList<Integer>> glossary;
+    private List<String> text;
+    private Map<String,Integer> wordFrequency;
 
-    public Glossary(ArrayList<String> text) {
-        glossary = new TreeMap<String, ArrayList<Integer>>();
+    public GlossaryApp(List<String> text) {
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null");
+        }
+
+        glossary = new TreeMap<>();
+        wordFrequency = new HashMap<>();
+        this.text = text;
+        
     }
 
-    public Glossary updateGlossary(ArrayList<String> text) {
-        // updateGlossary implementation
-        return new Glossary(text);
+    public GlossaryApp updateGlossary() {
+        GlossaryApp newGlossary = new GlossaryApp(text);
+
+        newGlossary.calculateWordFrequency();
+        newGlossary.insertEntriesToGlossary();
+
+        return newGlossary;
     }
 
-    public HashMap<String, Integer> checkWordFrequency() {
-        // checkWordFrequency implementation
-        return new HashMap<String, Integer>();
+    public void calculateWordFrequency() {
+        for (String paragraph : text) {
+            String[] words = paragraph.split(" ");
+            for (String word : words) {
+                wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+            }
+        }
+        //removes all entries with a value less then 3
+        wordFrequency.entrySet().removeIf(entry -> entry.getValue() < 3);
     }
 
-    public void insertEntriesToGlossary() {
-        // insertEntriesToGlossary implementation
+
+    public Map<String, Integer> getWordFrequency() {
+        return wordFrequency;
     }
 
-    public String findeParagraphIndex(String word) {
-        // findeParagraphIndex implementation
-        return "";
+
+    void insertEntriesToGlossary() {
+        for (String word : wordFrequency.keySet()) {
+            if (!glossary.containsKey(word)) {
+                ArrayList<Integer> indexes =  findParagraphIndexes(word);
+                glossary.put(word, indexes);
+            }
+        }
     }
 
-    public boolean isIndexValid() {
-        // isIndexValid implementation
-        return false;
+    ArrayList<Integer> findParagraphIndexes(String word) {
+        ArrayList<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < text.size(); i++) {
+            if (text.get(i).contains(word)) {
+                indexes.add(i);
+            }
+        }
+        //sorts the indexes
+        Collections.sort(indexes);
+        return indexes;
+    }
+  
+    boolean isIndexValid(int index) {
+        return index >= 0 && index < text.size();
     }
 
-    public TreeMap<String, ArrayList<Integer>> getGlossary() {
+    TreeMap<String, ArrayList<Integer>> getGlossary() {
         return glossary;
-    }
-
-    public void setGlossary(TreeMap<String, ArrayList<Integer>> glossary) {
-        this.glossary = glossary;
     }
 
 }
---
+++ src/test/java/com/NotFalse/app/GlossaryAppTest.java
@@ -0,0 +1,101 @@
+package com.NotFalse.app;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GlossaryAppTest {
+
+    private GlossaryApp glossary;
+
+    @BeforeEach
+    void setUp() {
+        List<String> text = new ArrayList<>();
+        text.add("This is a test paragraph.");
+        text.add("Another test paragraph.");
+        text.add("Another weird useless test paragraph");
+        glossary = new GlossaryApp(text);
+        // You should calculate word frequency here
+        glossary.calculateWordFrequency();
+    }
+
+    @Test
+    void testUpdateGlossary() {
+        List<String> newText = new ArrayList<>();
+        newText.add("custom test paragraph.");
+        newText.add("Another custom test paragraph.");
+        newText.add("Yet another custom test paragraph.");
+        newText.add("A totally new sentence with so many custom characters");
+        GlossaryApp newGlossaryApp = new GlossaryApp(newText);
+        newGlossaryApp.calculateWordFrequency();
+
+        Map<String, Integer> wordFrequency = newGlossaryApp.updateGlossary().getWordFrequency();
+
+        assertEquals(4, wordFrequency.get("custom"));
+        assertEquals(3, wordFrequency.get("paragraph."));
+        assertNull(wordFrequency.get("many"));
+        assertTrue(wordFrequency.containsKey("custom"));
+     
+    
+    }
+        
+
+    @Test
+    void testCheckWordFrequency() {
+        Map<String, Integer> wordFrequency = glossary.getWordFrequency();
+
+        assertTrue(wordFrequency.containsKey("test"));
+        assertFalse(wordFrequency.containsKey("Another"));
+        assertEquals(3, wordFrequency.get("test"));
+        assertNull(wordFrequency.get("Another"));
+        
+    }
+
+    @Test
+    void testInsertEntriesToGlossary() {
+        glossary.insertEntriesToGlossary();
+        TreeMap<String, ArrayList<Integer>> glossaryMap = glossary.getGlossary();
+        
+        assertTrue(glossaryMap.containsKey("test"));
+        assertFalse(glossaryMap.containsKey("Another"));
+        assertEquals(Arrays.asList(0, 1, 2), glossaryMap.get("test"));
+       
+    }
+
+    @Test
+    void testFindParagraphIndexes() {
+        // Test for a word that exists in the text
+        List<Integer> testIndexes = glossary.findParagraphIndexes("test");
+        assertEquals(Arrays.asList(0, 1, 2), testIndexes);
+
+        // Test for a word that doesn't exist in the text
+        List<Integer> nonexistentIndexes = glossary.findParagraphIndexes("nonexistent");
+        assertTrue(nonexistentIndexes.isEmpty());
+
+        // Test for a word that appears in some paragraphs
+        List<Integer> customIndexes = glossary.findParagraphIndexes("custom");
+        assertEquals(Arrays.asList(0, 1, 2), customIndexes); // Corrected expectation
+    }
+
+    @Test
+    void testIsIndexValid() {
+        assertTrue(glossary.isIndexValid(0));
+        assertTrue(glossary.isIndexValid(1));
+        assertTrue(glossary.isIndexValid(2));
+        assertFalse(glossary.isIndexValid(-1));
+        assertFalse(glossary.isIndexValid(3));
+    }
+
+    @Test
+    void testNullText() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new GlossaryApp(null);
+        });
+    }
+}

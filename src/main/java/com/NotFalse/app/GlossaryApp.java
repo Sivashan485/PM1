b522693c773d08b73f51e71467441d6b4da8496f
Michal Johnson <johnsmic@students.zhaw.ch>
2023-11-15 13:57:38 +0100

    added javadoc

--- src/main/java/com/NotFalse/app/GlossaryApp.java
+++ src/main/java/com/NotFalse/app/GlossaryApp.java
@@ -11,11 +11,19 @@ public class GlossaryApp {
     private TreeMap<String, ArrayList<Integer>> glossary;
     private Map<String,Integer> wordFrequency;
 
+    /**
+     * Constructor for GlossaryApp.
+     */
     public GlossaryApp() {
         glossary = new TreeMap<>();
         wordFrequency = new HashMap<>();
     }
 
+    /**
+     * Updates the glossary with the new text by creating a new glossary.
+     * @param text new text to be mapped
+     * @return returns a new GlossaryApp
+     */
     public GlossaryApp updateGlossary(ArrayList<String> text) {
         GlossaryApp newGlossary = new GlossaryApp();
         newGlossary.calculateWordFrequency(text);
@@ -23,6 +31,10 @@ public class GlossaryApp {
         return newGlossary;
     }
 
+    /**
+     * Calculates the frequency of each word, puts them into the wordFrequency Map and filters them.
+     * @param text text to be mapped
+     */
     void calculateWordFrequency(ArrayList<String> text) {
         for (String paragraph : text) {
             String[] words = paragraph.split(" ");
@@ -38,19 +50,28 @@ public class GlossaryApp {
     }
 
     /**
-     * Removes all entries with a value less than 3.
+     * Filters all entries out of the wordFrequency with a value less than 3.
      */
     void filterWordFrequency(){
         wordFrequency.entrySet().removeIf(entry -> entry.getValue() < 3);
     }
 
-
+    /**
+     * Inserts the entries to the glossary.
+     * @param text text to be mapped
+     */
     void insertEntriesToGlossary(ArrayList<String> text) {
         for (String word : wordFrequency.keySet()) {
             glossary.computeIfAbsent(word, k -> findParagraphIndexes(text, word));
         }
     }
 
+    /**
+     * Finds the indexes of the paragraphs which contain the word and returns them in an ArrayList.
+     * @param text text to be mapped
+     * @param word word to be searched
+     * @return returns a sorted ArrayList of the indexes of the paragraphs which contain the word
+     */
     ArrayList<Integer> findParagraphIndexes(ArrayList<String> text, String word) {
         ArrayList<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < text.size(); i++) {
@@ -68,13 +89,17 @@ public class GlossaryApp {
     }*/
 
     /**
-     * Returns the word frequency map.
-     * @return
+     * Gets the WordFrequency Map.
+     * @return returns the WordFrequency Map
      */
     public Map<String, Integer> getWordFrequency() {
         return wordFrequency;
     }
 
+    /**
+     * Gets the Glossary Map.
+     * @return returns the Glossary Map
+     */
     TreeMap<String, ArrayList<Integer>> getGlossary() {
         return glossary;
     }

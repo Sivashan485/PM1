620f38a1700825a3383a2a2a29f2a10fda1ec12b
ismhaj <ismhaj01@students.zhaw.ch>
2023-11-15 18:17:09 +0100

    Remove unnecessary import statement in InputReceiverTest.java

--- src/test/java/com/NotFalse/app/InputReceiverTest.java
+++ src/test/java/com/NotFalse/app/InputReceiverTest.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test.*;
 
 import java.io.ByteArrayInputStream;
 

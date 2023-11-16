eb9bc702562ec34565db5290b4c8f17666250ff6
Pascal Helfenberger <helfepa1@students.zhaw.ch>
2023-11-16 08:57:08 +0100

    repair testGetCommandsAsString

--- src/test/java/com/NotFalse/app/CommandsTest.java
+++ src/test/java/com/NotFalse/app/CommandsTest.java
@@ -36,11 +36,11 @@ public class CommandsTest {
         assertTrue(Commands.isCommand("unknown"));
         assertFalse(Commands.isCommand("not a command"));
     }
-    /*
-     * @Test
-     * public void testGetCommandsAsString() {
-     * assertEquals("Valid commands: exit, add, del, dummy, index, print, " +
-     * "replace, help, format_raw, format_fix", Commands.getCommandsAsString());
-     * }
-     */
+
+      @Test
+      public void testGetCommandsAsString() {
+      assertEquals("exit, add, del, dummy, index, print, " +
+      "replace, help, format_raw, format_fix", Commands.getAllCommands());
+      }
+
 }

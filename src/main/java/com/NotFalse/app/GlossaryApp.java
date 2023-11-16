456b8b77d68b9f2050ed47828559de8e22989ce9
Pascal Helfenberger <helfepa1@students.zhaw.ch>
2023-11-16 11:23:18 +0100

    - Java Doc in GlossaryApp

--- src/main/java/com/NotFalse/app/GlossaryApp.java
+++ src/main/java/com/NotFalse/app/GlossaryApp.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 /**
  * This class is responsible for creating the glossary and updating it.
+ * The glossary is a TreeMap, which contains the words as keys and the indexes of the paragraphs
+ * which contain the word as values.
  */
 public class GlossaryApp {
 
@@ -18,7 +20,7 @@ public class GlossaryApp {
 
     /**
      * Updates the glossary with the new text by creating a new glossary.
-     * 
+     *
      * @param text new text to be mapped
      * @return returns a new GlossaryApp
      */
@@ -32,7 +34,7 @@ public class GlossaryApp {
     /**
      * Calculates the frequency of each word, puts them into the wordFrequency Map
      * and filters them.
-     * 
+     *
      * @param text text to be mapped
      */
     Map<String, Integer> computeWordFrequency(List<String> text) {
@@ -53,10 +55,23 @@ public class GlossaryApp {
         return wordFrequency;
     }
 
+    /**
+     * Filters the paragraph by removing all the non-alphabetic characters and
+     * converting all the characters to lowercase.
+     *
+     * @param paragraphToFilter paragraph to be filtered
+     * @return returns the filtered paragraph
+     */
     String filterParagraph(String paragraphToFilter) {
         return paragraphToFilter.replaceAll("[^A-Za-z ]", " ").toLowerCase();
     }
 
+    /**
+     * Capitalizes the first letter of the word.
+     *
+     * @param word word to be capitalized
+     * @return returns the capitalized word
+     */
     String capitalizeFirstLetter(String word) {
         if (word.equals(null) || word.length() == 0)
             return word;
@@ -65,7 +80,7 @@ public class GlossaryApp {
 
     /**
      * Inserts the entries to the glossary.
-     * 
+     *
      * @param text text to be mapped
      */
     void insertEntriesToGlossary(List<String> text) {
@@ -78,18 +93,18 @@ public class GlossaryApp {
     /**
      * Finds the indexes of the paragraphs which contain the word and returns them
      * in an ArrayList.
-     * 
+     *
      * @param text text to be mapped
      * @param word word to be searched
      * @return returns a sorted ArrayList of the indexes of the paragraphs which
-     *         contain the word
+     * contain the word
      */
     List<Integer> findParagraphIndexes(List<String> text, String word) {
         List<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < text.size(); i++) {
             String cleanedParagraph = filterParagraph(text.get(i));
             if (cleanedParagraph.contains(word)) {
-                indexes.add(i+1);
+                indexes.add(i + 1);
             }
         }
         // sorts the indexes
@@ -99,7 +114,7 @@ public class GlossaryApp {
 
     /**
      * Gets the Glossary Map.
-     * 
+     *
      * @return returns the Glossary Map
      */
     TreeMap<String, List<Integer>> getGlossary() {

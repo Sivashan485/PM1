1028c069e4bb11f3edc3555f1479f1175d2a21be
ismhaj <ismhaj01@students.zhaw.ch>
2023-11-17 11:52:23 +0100

    Refactor InputReceiver class to improve input filtering and splitting

--- src/main/java/com/NotFalse/app/InputReceiver.java
+++ src/main/java/com/NotFalse/app/InputReceiver.java
@@ -12,14 +12,13 @@ public class InputReceiver {
     }
 
     public String filterInput(String textToFilter) {
-        // implementation
         return textToFilter.replaceAll(allowedRegex, "");
     }
 
     public String unsplittedText() {
         String inputText = input.nextLine();
-        if(inputText==null){
-            inputText =" ";
+        if (inputText == null) {
+            inputText = " ";
         }
         return inputText;
     }
@@ -30,9 +29,9 @@ public class InputReceiver {
         inputText = filterInput(inputText.trim());
         splitedtext[0] = inputText;
         for (Commands command : Commands.values()) {
-            if (inputText.toLowerCase().contains(command.getCommand()+" ")) {
+            if (inputText.toLowerCase().contains(command.getCommand() + " ")) {
                 // if a command contains then split it in two parts
-                splitedtext = inputText.toLowerCase().split(command.getCommand()+ " ");
+                splitedtext = inputText.toLowerCase().split(command.getCommand() + " ");
                 splitedtext[0] = command.getCommand();
             }
         }

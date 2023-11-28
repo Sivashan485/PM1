8def69190182bd339f63bf6ce2a3cf7603bbe436
ismhaj <ismhaj01@students.zhaw.ch>
2023-11-28 17:13:22 +0100

    commandTest added tests

--- src/test/java/com/NotFalse/app/CommandTest.java
+++ src/test/java/com/NotFalse/app/CommandTest.java
@@ -1,59 +1,122 @@
 package com.NotFalse.app;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CommandTest {
+import org.junit.jupiter.api.Test;
 
+class CommandTest {
+    
     @Test
-    public void testGetCommand() {
-        assertEquals("exit", Command.EXIT.getCommand());
-        assertEquals("add", Command.ADD.getCommand());
-        assertEquals("del", Command.DEL.getCommand());
-        assertEquals("dummy", Command.DUMMY.getCommand());
-        assertEquals("index", Command.INDEX.getCommand());
-        assertEquals("print", Command.PRINT.getCommand());
-        assertEquals("replace", Command.REPLACE.getCommand());
-        assertEquals("help", Command.HELP.getCommand());
-        assertEquals("format raw", Command.FORMAT_RAW.getCommand());
-        assertEquals("format fix", Command.FORMAT_FIX.getCommand());
-        assertEquals("unknown", Command.UNKNOWN.getCommand());
+    void testCommandExit() {
+        Command command = Command.EXIT;
+        assertEquals("exit", command.command);
+        assertFalse(command.commandHasIndex);
+        assertEquals("exit", command.getCommand());
+        assertEquals("EXIT", command.toString());
+        assertNotEquals("exitsufix", command.command);
+        assertNotEquals("prefixexit", command.command);
     }
 
     @Test
-    public void testGetAllCommands() {
-        assertEquals("exit, add, del, dummy, index, print, " +
-                "replace, help, format raw, format fix", Command.getAllCommands());
+    void testCommandAdd() {
+        Command command = Command.ADD;
+        assertEquals("add", command.command);
+        assertTrue(command.commandHasIndex);
+        assertEquals("add", command.getCommand());
+        assertEquals("ADD", command.toString());
+        assertNotEquals("addsufix", command.command);
+        assertNotEquals("prefixadd", command.command);
     }
 
     @Test
-    public void testKnownCommand() {
-        assertEquals(Command.HELP, Command.parseCommand("Help"));
+    void testCommandDel() {
+        Command command = Command.DEL;
+        assertEquals("del", command.command);
+        assertTrue(command.commandHasIndex);
+        assertEquals("del", command.getCommand());
+        assertEquals("DEL", command.toString());
+        assertNotEquals("delsufix", command.command);
+        assertNotEquals("prefixdel", command.command);
     }
 
     @Test
-    public void testUnknownCommand() {
-        assertEquals(Command.UNKNOWN, Command.parseCommand("nonExistentCommand"));
-    }
-
-    /*
-     * ->> fix bug -> this test is not working
-     * 
-     * @Test
-     * public void testNullInput() {
-     * Commands.lookupCommand(null);
-     * }
-     */
-
-    @Test
-    public void testCaseSensitivity() {
-        assertEquals(Command.EXIT, Command.parseCommand("EXIT"));
-        assertEquals(Command.EXIT, Command.parseCommand("exit"));
+    void testCommandDummy() {
+        Command command = Command.DUMMY;
+        assertEquals("dummy", command.command);
+        assertTrue(command.commandHasIndex);
+        assertEquals("dummy", command.getCommand());
+        assertEquals("DUMMY", command.toString());
+        assertNotEquals("dummysufix", command.command);
+        assertNotEquals("prefixdummy", command.command);
     }
 
     @Test
-    public void testEmptyString() {
-        assertEquals(Command.UNKNOWN, Command.parseCommand(""));
+    void testCommandIndex() {
+        Command command = Command.INDEX;
+        assertEquals("index", command.command);
+        assertFalse(command.commandHasIndex);
+        assertEquals("index", command.getCommand());
+        assertEquals("INDEX", command.toString());
+        assertNotEquals("indexsufix", command.command);
+        assertNotEquals("prefixindex", command.command);
+    }
+
+    @Test
+    void testCommandPrint() {
+        Command command = Command.PRINT;
+        assertEquals("print", command.command);
+        assertFalse(command.commandHasIndex);
+        assertEquals("print", command.getCommand());
+        assertEquals("PRINT", command.toString());
+        assertNotEquals("printsufix", command.command);
+        assertNotEquals("prefixprint", command.command);
+    }
+
+    @Test
+    void testCommandReplace() {
+        Command command = Command.REPLACE;
+        assertEquals("replace", command.command);
+        assertTrue(command.commandHasIndex);
+        assertEquals("replace", command.getCommand());
+        assertEquals("REPLACE", command.toString());
+        assertNotEquals("replacesufix", command.command);
+        assertNotEquals("prefixreplace", command.command);
+    }
+
+    @Test
+    void testCommandHelp() {
+        Command command = Command.HELP;
+        assertEquals("help", command.command);
+        assertFalse(command.commandHasIndex);
+        assertEquals("help", command.getCommand());
+        assertEquals("HELP", command.toString());
+        assertNotEquals("helpsufix", command.command);
+        assertNotEquals("prefixhelp", command.command);
+    }
+
+    @Test
+    void testCommandFormatRaw() {
+        Command command = Command.FORMAT_RAW;
+        assertEquals("format raw", command.command);
+        assertFalse(command.commandHasIndex);
+        assertEquals("format raw", command.getCommand());
+        assertEquals("FORMAT_RAW", command.toString());
+        assertNotEquals("format rawsufix", command.command);
+        assertNotEquals("prefixformat raw", command.command);
+    }
+
+    @Test
+    void testCommandFormatFix() {
+        Command command = Command.FORMAT_FIX;
+        assertEquals("format fix", command.command);
+        assertTrue(command.commandHasIndex);
+        assertEquals("format fix", command.getCommand());
+        assertEquals("FORMAT_FIX", command.toString());
+        assertNotEquals("formatsufix fixsufix", command.command);
+        assertNotEquals("prefixformat prefixfix", command.command);
     }
 
 }

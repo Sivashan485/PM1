ab796cc421b0e97ee9a6f00c6e013c87396c7ab8
ismhaj <ismhaj01@students.zhaw.ch>
2023-12-07 12:25:56 +0100

    refractored and added javadoc to InputReceiver

--- src/main/java/com/texteditor/app/InputReceiver.java
+++ src/main/java/com/texteditor/app/InputReceiver.java
@@ -7,14 +7,12 @@ import java.util.Scanner;
  */
 public class InputReceiver {
 
-    private static final String ALLOWED_REGEX = "[^A-Za-zäöüÄÖÜ 0-9 / .,:;\\-!?'\\\\()\\\"%@+*{}\\\\\\\\&#$\\[\\]]";
+    private static final String DISALLOWED_CHARACTERS_REGEX = "[^A-Za-zäöüÄÖÜ 0-9 / .,:;\\-!?'\\\\()\\\"%@+*{}\\\\\\\\&#$\\[\\]]";
     private final Scanner userInput;
     private String userCommand;
     private Integer userIndex;
     private String restPart;
     private boolean isIndexValid;
-
-
 
     /**
      * Constructor for InputReceiver.
@@ -26,8 +24,8 @@ public class InputReceiver {
 
     /**
      * Resets the values of the instance variables associated with this class.
-     * This method sets IsIndexValid to true, index to null, command to an empty string,
-     * and indexIsNull to false.
+     * This method sets userIndex to null, userCommand to an empty string and
+     * restPart to an empty string.
      */
     void resetValues() {
         userIndex = null;
@@ -36,34 +34,36 @@ public class InputReceiver {
     }
 
     /**
-     * Receives the input from the user and filters it.
+     * Reads the user input, removes all characters that are not allowed and
+     * returns the filtered input.
      *
-     * @return returns the filtered input text
+     * @return The filtered input text.
      */
     String readAndFilterUserInput() {
         String inputText = userInput.nextLine();
-        return inputText.replaceAll(ALLOWED_REGEX, "");
+        return inputText.replaceAll(DISALLOWED_CHARACTERS_REGEX, "");
     }
 
     /**
-     * Splits the user input into a command and its arguments.
+     * Splits the user input into a command and its arguments. If the command
+     * requires an index and it's not provided, it sets the command to "unknown".
      */
     public void parseInput() {
         String input = readAndFilterUserInput();
         userCommand = extractCommand(input);
         restPart = input.substring(userCommand.length()).trim();
-        if(!validateAndSetIndex(userCommand, restPart)){
+        if (!validateAndSetIndex(userCommand, restPart)) {
             userCommand = "unknown";
         }
     }
 
-
-
     /**
-     * Extracts the command from the input
+     * Extracts the command from the input. If the command is not recognized, it
+     * tries to combine the first two words and parse them as a command.
      *
-     * @param userInput adfisjoij
-     * @return returns the command
+     * @param userInput The user's input.
+     * @return The recognized command, or an empty string if no command is
+     *         recognized.
      */
     String extractCommand(String userInput) {
         String[] userInputPartition = userInput.toLowerCase().split(" ");
@@ -77,37 +77,60 @@ public class InputReceiver {
         return "";
     }
 
+    /**
+     * Validates the command and sets the index if required. If the command does not
+     * require an index, it checks if the rest part of the input is empty.
+     *
+     * @param command  The command to validate.
+     * @param restPart The rest part of the user input.
+     * @return True if the command does not require an index and the rest part is
+     *         empty, false otherwise.
+     */
     private boolean validateAndSetIndex(String command, String restPart) {
-        if(ApplicationCommand.parseCommand(command).getCommandHasIndex()){
-            if(restPart != null && !restPart.isEmpty()){
+        if (ApplicationCommand.parseCommand(command).getCommandHasIndex()) {
+            if (restPart != null && !restPart.isEmpty()) {
                 handleIndexCommand();
-            }else{
+            } else {
                 setUserIndex();
             }
-        } else{
+        } else {
             return "".equals(restPart);
         }
         return true;
     }
 
-    private void handleIndexCommand(){
+    /**
+     * Handles the index command.
+     * If the rest part of the command matches a numeric pattern, it sets the user
+     * index and validates it.
+     * If the rest part does not match the numeric pattern, it invalidates the
+     * index.
+     */
+    private void handleIndexCommand() {
         if (restPart.matches("^[0-9]+$")) {
             setUserIndex();
             isIndexValid = true;
-        }else{
+        } else {
             isIndexValid = false;
         }
     }
 
-    private void setUserIndex(){
+    /**
+     * Sets the user index from the rest part of the command.
+     * If the rest part is not null and can be parsed as an integer, it sets the
+     * user index to that integer.
+     * If the rest part is not an integer, it catches the NumberFormatException and
+     * leaves the user index as null.
+     */
+    private void setUserIndex() {
         isIndexValid = true;
         userIndex = null;
-        try{
+        try {
             if (restPart != null) {
                 userIndex = Integer.parseInt(restPart);
             }
-        }catch (NumberFormatException e){
-            //left empty
+        } catch (NumberFormatException e) {
+            // left empty
         }
     }
 
@@ -121,7 +144,12 @@ public class InputReceiver {
         return userIndex == null;
     }
 
-    public boolean getIsIndexValid(){
+    /**
+     * Returns the validity of the index.
+     *
+     * @return true if the index is valid, false otherwise.
+     */
+    public boolean getIsIndexValid() {
         return isIndexValid;
     }
 
@@ -130,7 +158,7 @@ public class InputReceiver {
      *
      * @return The current command as a string.
      */
-    public String getUserCommand () {
+    public String getUserCommand() {
         return userCommand;
     }
 
@@ -139,13 +167,16 @@ public class InputReceiver {
      *
      * @return The current index as an integer.
      */
-    public Integer getUserIndex () {
+    public Integer getUserIndex() {
         return userIndex;
     }
 
+    /**
+     * Retrieves the rest part of the command.
+     *
+     * @return The rest part of the command as a string.
+     */
     String getRestPart() {
         return restPart;
     }
-
-
 }
